17003f9343e9183d9b5144a6c83e692d2136e601
Sam <sambendat@gmail.com>
2022-05-19 14:39:44 -0500

    js height change

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ window.handleIFrameMessage = function(e) {
       iframe.scrollIntoView();
       break;
     case "setHeight":
-      iframe.style.height = (args[1] - 75) + "px";
+      iframe.style.height = (args[1] - 100) + "px";
       break;
     case "collapseErrorPage":
       if (iframe.clientHeight > window.innerHeight) {
